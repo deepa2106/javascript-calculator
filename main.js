cbf5336a1d9ae5389a84a86798500e4c8f712023
Tech-A03 <tech-a03@Tech-A03s-MacBook-Air-2.local>
2017-10-30 19:05:34 +0000

    Added options to choose calculator

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 var typeOfCalculator = prompt("Enter a for an advance calculator and b for a basic calculator");
 
 var number1 = parseFloat(prompt("Enter first number: "));
+
+if(typeOfCalculator == "a") {
+  var operation = prompt("Type ^ for power and sqrt for square root");
+} else if (typeOfCalculator == "b") {
+  var operation = prompt("Type operation: +-*/");
+} else {
+  console.log("Invalid option.");
+}
